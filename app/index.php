708071b52fd0ca5c3d09e76459844b92ea133238
mustakrakishe <mustakrkish@gmail.com>
2023-04-13 19:05:36 +0300

    feat(core): create routing

--- app/index.php
+++ app/index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'Hello World!';
+switch ($_SERVER['REQUEST_URI']) {
+    case '/':
+        echo 'Hi, Guest!';
+        break;
+
+    default:
+        http_response_code(404);
+        echo '404 Page Not Found';
+}
