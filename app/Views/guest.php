c67612fae9d9fb03b4eb7401f1bbebc757f391fa
mustakrakishe <mustakrkish@gmail.com>
2023-04-14 19:52:29 +0300

    feat(guest): create a guest page

---
+++ app/Views/guest.php
@@ -0,0 +1,5 @@
+<?php include ('Views/layout/header.php'); ?>
+
+<div class="text-center">Please, login or register!</div>
+
+<?php include ('Views/layout/footer.php'); ?>
---
+++ app/Views/layout/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title><?= $title ?></title>
+
+    <link rel="stylesheet" href="Views/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container py-3">
+        <header>
+            <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+                <a href="/" class="d-flex align-items-center link-body-emphasis text-decoration-none">
+                    <span class="fs-4">Polls</span>
+                </a>
+
+                <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+                    <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="#">Login</a>
+                    <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="#">Register</a>
+                </nav>
+            </div>
+        </header>
+
+        <main>
