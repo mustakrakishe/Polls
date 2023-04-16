312c4b275145c5f0b030cd3650c1e026289baf0a
mustakrakishe <mustakrkish@gmail.com>
2023-04-16 16:03:07 +0300

    feat(auth view): add a register form

--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -1,7 +1,25 @@
 <?php $title = 'Register'; ?>
 
-<?php include ('Views/layout/header.php'); ?>
+<?php include('Views/layout/header.php'); ?>
 
-<div class="text-center">Register</div>
+<div class="text-center">
+  <div class="w-100 m-auto" style="max-width: 300px;">
+    <form>
+      <h1 class="h3 mb-3 fw-normal">Please login</h1>
 
-<?php include ('Views/layout/footer.php'); ?>
+      <div class="form-floating mb-1">
+        <input type="email" class="form-control" id="floatingInput" placeholder=" " required>
+        <label for="floatingInput">Email address</label>
+      </div>
+
+      <div class="form-floating mb-3">
+        <input type="password" class="form-control" id="floatingPassword" placeholder=" " required>
+        <label for="floatingPassword">Password</label>
+      </div>
+
+      <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+    </form>
+  </div>
+</div>
+
+<?php include('Views/layout/footer.php'); ?>
