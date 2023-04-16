a111b9e7b6b1f9458ddaf657c2a785a57017e9d5
mustakrakishe <mustakrkish@gmail.com>
2023-04-17 00:02:15 +0300

    feat(build): set env variables from .env

--- html/index.php
+++ html/index.php
@@ -5,6 +5,10 @@ require __DIR__ . '/vendor/autoload.php';
 use Core\Application;
 use Core\Router;
 
+foreach (file('.env', FILE_SKIP_EMPTY_LINES) as $assigment) {
+    putenv($assigment);
+}
+
 $router = new Router;
 
 include 'routes/web.php';
