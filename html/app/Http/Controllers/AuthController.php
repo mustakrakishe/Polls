3e7067c5332cc5fd2a015113473c09886b5ac16b
mustakrakishe <mustakrkish@gmail.com>
2023-04-17 03:44:58 +0300

    feat(auth): save to db a registered user

--- html/app/Http/Controllers/AuthController.php
+++ html/app/Http/Controllers/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
 use Core\Controller;
 
 class AuthController extends Controller
@@ -11,8 +13,16 @@ class AuthController extends Controller
         $this->view->render('auth.register');
     }
 
-    public function register()
+    public function register(RegisterRequest $request)
     {
+        $user = new User;
+
+        $user->create([
+            'email'         => $request->input('email'),
+            'password_hash' => md5($request->input('password')),
+            'token_hash'    => md5(random_bytes(getenv('API_TOKEN_LENGTH'))),
+        ]);
+
         $this->view->render('auth.register');
     }
 }
