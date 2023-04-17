eda5ac475987823c28d10de0293f46ec5ce82951
mustakrakishe <mustakrkish@gmail.com>
2023-04-17 15:39:36 +0300

    fix(guest view): add a login link

--- html/resources/views/guest.php
+++ html/resources/views/guest.php
@@ -2,6 +2,6 @@
 
 <?php include 'layout' . DIRECTORY_SEPARATOR . 'header.php'; ?>
 
-<div class="text-center">Please, login or <a href="/register">register</a>!</div>
+<div class="text-center">Please, <a href="/login">login</a> or <a href="/register">register</a>!</div>
 
 <?php include 'layout' . DIRECTORY_SEPARATOR . 'footer.php'; ?>
