147fa0d1a2da92007738f201af0a7188984e0d24
mustakrakishe <mustakrkish@gmail.com>
2023-04-17 10:38:24 +0300

    feat(user): create a personal page view

--- html/app/Http/Controllers/UserController.php
+++ html/app/Http/Controllers/UserController.php
@@ -8,6 +8,6 @@ class UserController extends Controller
 {
     public function index()
     {
-        echo 'Personal';
+        $this->view->render('user.personal');
     }
 }
---
+++ html/resources/views/user/personal.php
@@ -0,0 +1,21 @@
+<?php $title = 'Personal'; ?>
+
+<?php 
+  include join(DIRECTORY_SEPARATOR, [
+    __DIR__,
+    '..',
+    'layout',
+    'header.php',
+  ]);
+?>
+
+<h1 class="h3 mb-3 fw-normal">Personal</h1>
+
+<?php 
+  include join(DIRECTORY_SEPARATOR, [
+    __DIR__,
+    '..',
+    'layout',
+    'footer.php',
+  ]);
+?>
