c6a73fd8929105f7508d1fa2a19e1b586254fd82
mustakrakishe <mustakrkish@gmail.com>
2023-04-17 15:35:25 +0300

    feat(core validation): create a unique rule

--- html/core/Validation/Concerns/Rules.php
+++ html/core/Validation/Concerns/Rules.php
@@ -2,6 +2,8 @@
 
 namespace Core\Validation\Concerns;
 
+use Core\Model;
+
 trait Rules
 {
     protected function required($value)
@@ -13,9 +15,18 @@ trait Rules
     {
         return (bool) filter_var($value, FILTER_VALIDATE_EMAIL);
     }
-
+    
     protected function min(string $value, int $limit)
     {
         return strlen($value) >= $limit;
+    }
+
+    protected function unique(string $value, string $table, string $column)
+    {
+        $record = Model::table($table)->where([
+            [$column, $value],
+        ])->first();
+
+        return is_null($record);
     }
 }
