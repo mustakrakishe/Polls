d1bd4c3d7eb06a3727d7f1aa374b12b4cc11167a
mustakrakishe <mustakrkish@gmail.com>
2023-04-18 14:51:59 +0300

    refactor(core app)
    - clean a route action run procedure;
    - make it bind a controller method required request instance.

--- html/core/Application.php
+++ html/core/Application.php
@@ -2,16 +2,27 @@
 
 namespace Core;
 
+use Core\Contracts\AbstractRequest;
 use Core\Contracts\RouterInterface;
+use Core\Contracts\ViewInterface;
+use Core\ORM\Contracts\ModelInterface;
+use Core\Validation\Contracts\ValidatorInterface;
 use Exception;
+use ReflectionMethod;
 
 class Application
 {
     protected RouterInterface $router;
+    protected ViewInterface $view;
+    protected ModelInterface $model;
+    protected ValidatorInterface $validator;
 
-    public function __construct(RouterInterface $router)
+    public function __construct(RouterInterface $router, ModelInterface $model, ViewInterface $view, ValidatorInterface $validator)
     {
-        $this->router = $router;
+        $this->router       = $router;
+        $this->view         = $view;
+        $this->model        = $model;
+        $this->validator    = $validator;
     }
 
     public function run()
@@ -22,7 +33,9 @@ class Application
         $method = $_SERVER['REQUEST_METHOD'];
 
         try {
-            $this->router->route($url, $method);
+            $this->runAction(
+                $this->router->route($url, $method)
+            );
         } catch (Exception $e) {
             if ($e->getCode() === 422) {
                 $header = 'Location: ' . $_SERVER['HTTP_REFERER'];
@@ -37,5 +50,48 @@ class Application
             http_response_code($e->getCode());
             echo 'Error ' . $e->getCode() . ': ' . $e->getMessage();
         }
+    }
+
+    protected function runAction($action)
+    {
+        if (is_array($action)) {
+            $action     = $this->prepareCallback($action);
+            $request    = $this->makeRequiredRequest($action);
+        }
+
+        call_user_func($action, $request ?? null);
+    }
+
+    protected function prepareCallback(array $callback)
+    {
+        $callback[0] = $this->newController($callback[0]);
+
+        return $callback;
+    }
+
+    protected function newController(string $class) : Controller
+    {
+        return new $class($this->model, $this->view);
+    }
+
+    protected function makeRequiredRequest(array $callback)
+    {
+        $method         = new ReflectionMethod($callback[0], $callback[1]);
+        $parameters     = $method->getParameters();
+
+        if ($parameters === []) {
+            return null;
+        }
+
+        $requestArgument    = $parameters[0];
+        $requestClass       = $requestArgument->getType()
+                                              ->getName();
+
+        return $this->newRequest($requestClass);
+    }
+
+    protected function newRequest(string $class) : AbstractRequest
+    {
+        return new $class($_POST, $this->validator);
     }
 }
--- html/index.php
+++ html/index.php
@@ -3,7 +3,12 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use Core\Application;
+use Core\Connection;
+use Core\ORM\Model;
+use Core\ORM\QueryBuilder;
 use Core\Router;
+use Core\Validation\Validator;
+use Core\View;
 
 foreach (file('.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES) as $assigment) {
     putenv($assigment);
@@ -13,8 +18,23 @@ $router = new Router;
 
 include 'routes/web.php';
 
+$connection     = new Connection(
+                    getenv('DB_DRIVER'),
+                    getenv('DB_HOST'),
+                    getenv('DB_PORT'),
+                    getenv('DB_DATABASE'),
+                    getenv('DB_USERNAME'),
+                    getenv('DB_PASSWORD')
+                );
+$queryBuilder   = new QueryBuilder($connection);
+$model          = new Model($queryBuilder);
+$validator      = new Validator($model);
+
 $app = new Application(
-    $router
+    $router,
+    $model,
+    new View,
+    $validator
 );
 
 $app->run();
